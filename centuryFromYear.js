7ee02b3ffe8fd698b57447ef277e110417175572
Nick Verdin <nv1028@aggienetwork.com>
2020-03-11 15:30:57 -0500

    added comments

--- centuryFromYear.js
+++ centuryFromYear.js
@@ -1,3 +1,6 @@
 function centuryFromYear(year) {
+    //returns the century when given a year
+    // 1700 returns 17
+    // 1771 returns 18
     return Number.isInteger(year/100) ? year/100 : Math.ceil(year/100)
 };
